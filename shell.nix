57dfa5a40ada28cf9ce2eb78b63d212a042017b5
Jeff Stein <jeffstein@gmail.com>
2024-10-08 09:22:46 -0600

    build: adding a shell.nix so you can do nix-shell to work on this repo

---
+++ shell.nix
@@ -0,0 +1,37 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  pythonEnv = pkgs.python312.withPackages (ps: with ps; [
+    ps.pip
+    ps.numpy
+    ps.uv
+
+    # Include any additional Python packages here
+  ]);
+in
+pkgs.mkShell {
+  buildInputs = [
+    pythonEnv
+    pkgs.poetry
+    pkgs.commitlint-rs
+    pkgs.nodejs  # Add Node.js here
+
+  ];
+
+  shellHook = ''
+    export POETRY_HOME=$(pwd)/.poetry
+    export PATH=$POETRY_HOME/bin:$PATH
+
+    # Optional: install Poetry if not already installed
+    if [ ! -d "$POETRY_HOME" ]; then
+      poetry config virtualenvs.in-project true
+      poetry install
+    fi
+    # Activate the Poetry environment
+    python -m venv .venv
+    source .venv/bin/activate
+    pre-commit install
+    pre-commit install --hook-type commit-msg
+    npm install @commitlint/config-conventional
+  '';
+}
