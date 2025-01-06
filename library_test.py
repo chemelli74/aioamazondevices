f92f1877fc93b03cf3b91859266de3759d2c03f7
Simone Chemelli <simone.chemelli@gmail.com>
2025-01-06 20:42:23 +0000

    chore: introduce auth_check_status()

--- library_test.py
+++ library_test.py
@@ -133,7 +133,7 @@ async def main() -> None:
             raise
     except AmazonError:
         await api.close()
-        sys.exit(1)
+        sys.exit(2)
 
     print("Logged-in.")
 
@@ -149,6 +149,10 @@ async def main() -> None:
     print("-" * 20)
 
     save_to_file(f"{SAVE_PATH}/output-devices.json", devices)
+
+    if not (await api.auth_check_status()):
+        print("!!! Error: Session not authenticated !!!")
+        sys.exit(3)
 
     device = next(iter(devices.values()))
     payload = await api.send_announcement(device, "Test message from new library")
