6e512427807f38c995a59c2a09e2b9e30f21ca03
Simone Chemelli <simone.chemelli@gmail.com>
2025-05-29 16:53:05 +0300

    chore: improve script (#200)

--- scripts/sort_DEVICE_TYPE_TO_MODEL.py
+++ scripts/sort_DEVICE_TYPE_TO_MODEL.py
@@ -1,13 +1,21 @@
 """Script to sort DEVICE_TYPE_TO_MODEL."""
 
 import sys
+from pathlib import Path
 from typing import Any
-
-import orjson
 
 sys.path.append("/workspaces/aioamazondevices/src/aioamazondevices")
 from const import DEVICE_TYPE_TO_MODEL
 
 sorted_dict: dict[str, dict[str, Any]] = dict(sorted(DEVICE_TYPE_TO_MODEL.items()))
+destination_file = "scripts/sorted_device_model.json"
 
-print(orjson.dumps(sorted_dict, option=orjson.OPT_INDENT_2))
+file = Path.open(Path(destination_file), mode="w", encoding="utf-8")
+
+for key, value in sorted_dict.items():
+    file.write(f'  "{key}": {{\n')
+    file.write(f'    "model": "{value["model"]}",\n')
+    file.write(f'    "hw_version": "{value["hw_version"]}",\n')
+    file.write("},\n")
+
+file.close()
