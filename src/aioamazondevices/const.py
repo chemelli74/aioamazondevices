c1ef29d9a4ebd0f01bf48db2059b5ba5ce5581d8
jameson_uk <1040621+jamesonuk@users.noreply.github.com>
2025-06-12 09:58:57 +0100

    Update const.py
    New models as per https://github.com/chemelli74/aioamazondevices/issues/245

--- src/aioamazondevices/const.py
+++ src/aioamazondevices/const.py
@@ -197,4 +197,20 @@ DEVICE_TYPE_TO_MODEL: dict[str, dict[str, str | None]] = {
         "model": "Echo Plus",
         "hw_version": "Gen2",
     },
+    "A1RTAM01W29CUP": {
+        "model": "Alexa App for PC",
+        "hw_version": None,
+    },
+    "ADVBD696BHNV5": {
+        "model": "Fire TV Stick",
+        "hw_versoin": "Gen1",
+    },
+    "A1TD5Z1R8IWBHA": {
+        "model": "Fire Table HD 8",
+        "hw_version": "Gen12",
+    },
+    "A15ERDAKK5HQQG": {
+        "manufacturer": "Sonos",
+        "model": "Sonos Play 5",
+        "hw_version": "Gen1",
 }
