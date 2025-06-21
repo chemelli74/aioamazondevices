35983aa7d4bb6256f96d671be9c73bcc990a6947
Luis Chavez <lualchar@gmail.com>
2025-06-21 03:52:20 -0600

    fix: add Mexico settings (#240)
    Co-authored-by: Simone Chemelli <simone.chemelli@gmail.com>

--- src/aioamazondevices/const.py
+++ src/aioamazondevices/const.py
@@ -29,6 +29,9 @@ DOMAIN_BY_ISO3166_COUNTRY = {
     "jp": {
         "domain": "co.jp",
     },
+    "mx": {
+        "domain": "com.mx",
+    },
     "nz": {
         "domain": "com.au",
         "openid.assoc_handle": DEFAULT_ASSOC_HANDLE,
