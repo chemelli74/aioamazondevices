b15fb02c50dec19d0bbcaca671e4892265dee253
semantic-release <semantic-release>
2025-06-23 12:41:17 +0000

    3.2.0-rc.1
    Automatically generated by python-semantic-release

--- src/aioamazondevices/__init__.py
+++ src/aioamazondevices/__init__.py
@@ -1,6 +1,10 @@
 """aioamazondevices library."""
 
+<<<<<<< HEAD
 __version__ = "3.3.1"
+=======
+__version__ = "3.2.0-rc.1"
+>>>>>>> cd29f58 (3.2.0-rc.1)
 
 
 from .api import AmazonDevice, AmazonEchoApi
