441ebc11564961a67cda580971a1b79ead47ff7c
Simone Chemelli <simone.chemelli@gmail.com>
2025-06-26 01:17:46 +0000

    chore: merge

--- src/aioamazondevices/__init__.py
+++ src/aioamazondevices/__init__.py
@@ -1,10 +1,14 @@
 """aioamazondevices library."""
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 __version__ = "3.3.1"
 =======
 __version__ = "3.2.0-rc.1"
 >>>>>>> cd29f58 (3.2.0-rc.1)
+=======
+__version__ = "3.1.19"
+>>>>>>> 879e3af (chore: merge)
 
 
 from .api import AmazonDevice, AmazonEchoApi
