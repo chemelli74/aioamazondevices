7dd3036bb48908a7a62736ed9e78f155ba56c810
Simone Chemelli <simone.chemelli@gmail.com>
2025-06-29 15:18:42 +0000

    fix: align signin payload to mobile app

--- src/aioamazondevices/api.py
+++ src/aioamazondevices/api.py
@@ -146,6 +146,15 @@ class AmazonEchoApi:
         self.session: ClientSession
         self._devices: dict[str, Any] = {}
 
+        lang_object = Language.make(territory=self._login_country_code.upper())
+        lang_maximized = lang_object.maximize()
+        self._language = f"{lang_maximized.language}-{lang_maximized.region}"
+        _LOGGER.debug(
+            "Initialize library with domain <%s> nad language <%s>",
+            self._domain,
+            self._language,
+        )
+
     def _load_website_cookies(self) -> dict[str, str]:
         """Get website cookies, if avaliables."""
         if not self._login_stored_data:
@@ -210,22 +219,23 @@ class AmazonEchoApi:
         code_challenge = self._create_s256_code_challenge(code_verifier)
 
         oauth_params = {
-            "openid.oa2.response_type": "code",
-            "openid.oa2.code_challenge_method": "S256",
-            "openid.oa2.code_challenge": code_challenge,
             "openid.return_to": f"https://www.amazon.{self._domain}/ap/maplanding",
+            "openid.oa2.code_challenge_method": "S256",
             "openid.assoc_handle": self._assoc_handle,
             "openid.identity": "http://specs.openid.net/auth/2.0/identifier_select",
+            "pageId": self._assoc_handle,
             "accountStatusPolicy": "P1",
             "openid.claimed_id": "http://specs.openid.net/auth/2.0/identifier_select",
             "openid.mode": "checkid_setup",
             "openid.ns.oa2": "http://www.amazon.com/ap/ext/oauth/2",
             "openid.oa2.client_id": f"device:{client_id}",
+            "language": self._language,
             "openid.ns.pape": "http://specs.openid.net/extensions/pape/1.0",
+            "openid.oa2.code_challenge": code_challenge,
             "openid.oa2.scope": "device_auth_access",
-            "forceMobileLayout": "true",
             "openid.ns": "http://specs.openid.net/auth/2.0",
             "openid.pape.max_auth_age": "0",
+            "openid.oa2.response_type": "code",
         }
 
         return (
@@ -842,10 +852,6 @@ class AmazonEchoApi:
         message_source: AmazonMusicSource | None = None,
     ) -> None:
         """Send message to specific device."""
-        lang_object = Language.make(territory=self._login_country_code.upper())
-        lang_maximized = lang_object.maximize()
-        locale = f"{lang_maximized.language}-{lang_maximized.region}"
-
         if not self._login_stored_data:
             _LOGGER.warning("Trying to send message before login")
             return
@@ -853,7 +859,7 @@ class AmazonEchoApi:
         base_payload = {
             "deviceType": device.device_type,
             "deviceSerialNumber": device.serial_number,
-            "locale": locale,
+            "locale": self._language,
             "customerId": device.device_owner_customer_id,
         }
 
@@ -888,7 +894,7 @@ class AmazonEchoApi:
                 "expireAfter": "PT5S",
                 "content": [
                     {
-                        "locale": locale,
+                        "locale": self._language,
                         "display": {
                             "title": "Home Assistant",
                             "body": message_body,
