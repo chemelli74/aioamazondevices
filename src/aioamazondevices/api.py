76f78525cc3880f1bf7ab6913316a13c5743ae3b
jameson_uk <1040621+jamesonuk@users.noreply.github.com>
2025-08-14 13:19:34 +0100

    chore: follow up of #443 (#446)

--- src/aioamazondevices/api.py
+++ src/aioamazondevices/api.py
@@ -587,9 +587,9 @@ class AmazonEchoApi:
         resp_me_json = await resp_me.json()
         amazon_market = resp_me_json["marketPlaceDomainName"]
 
-        if amazon_market != self._market:
+        if amazon_market not in self._market:
             _LOGGER.warning(
-                "Selected country <%s> doesn't matches Amazon API reply:\n%s\n vs \n%s",
+                "Selected country <%s> doesn't match Amazon API reply:\n%s\n vs \n%s",
                 self._login_country_code.upper(),
                 {"input ": self._market},
                 {"amazon": amazon_market},
