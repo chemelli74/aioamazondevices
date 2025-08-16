98cace191f01705647e3bb3d6768fe84528a7c92
Simone Chemelli <simone.chemelli@gmail.com>
2025-08-16 15:40:05 +0000

    chore: fix for domain swap

--- src/aioamazondevices/const.py
+++ src/aioamazondevices/const.py
@@ -100,6 +100,9 @@ DEFAULT_HEADERS = {
 DEFAULT_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0"  # noqa: E501
 CSRF_COOKIE = "csrf"
 
+REFRESH_ACCESS_TOKEN = "access_token"  # noqa: S105
+REFRESH_AUTH_COOKIES = "auth_cookies"
+
 NODE_DEVICES = "devices"
 NODE_DO_NOT_DISTURB = "doNotDisturbDeviceStatusList"
 NODE_PREFERENCES = "devicePreferences"
