21de66e4be577c62a588e05f2930d0e9e2308a1e
Jameson_UK <1040621+jamesonuk@users.noreply.github.com>
2025-08-25 12:28:15 +0000

    feat: replace sensor endpoint with graphql

--- src/aioamazondevices/const.py
+++ src/aioamazondevices/const.py
@@ -76,19 +76,8 @@ URI_QUERIES = {
 }
 
 URI_SIGNIN = "/ap/signin"
-URI_IDS = "/api/phoenix"
-URI_SENSORS = "/api/phoenix/state"
+URI_NEXUS_GRAPHQL = "/nexus/v1/graphql"
 
-SENSORS = [
-    "babyCryDetectionState",
-    "beepingApplianceDetectionState",
-    "coughDetectionState",
-    "dogBarkDetectionState",
-    "humanPresenceDetectionState",
-    "illuminance",
-    "temperature",
-    "waterSoundsDetectionState",
-]
 SENSOR_STATE_OFF = "NOT_DETECTED"
 
 # File extensions
