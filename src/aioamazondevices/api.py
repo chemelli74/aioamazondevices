f2c3678a17a4b4032f99e2f06ce578d65fd17982
Simone Chemelli <simone.chemelli@gmail.com>
2025-09-13 15:45:41 +0200

    refactor: cleaner code

--- src/aioamazondevices/api.py
+++ src/aioamazondevices/api.py
@@ -639,7 +639,7 @@ class AmazonEchoApi:
                 scale=None,
             )
         for feature in endpoint.get("features", {}):
-            feature_property = (feature.get("properties") or [None])[0] or {}
+            feature_property = (feature.get("properties") or [{}])[0]
             if (sensor := SENSORS.get(feature["name"])) is None:
                 # Skip sensors that are not in the predefined list
                 continue
