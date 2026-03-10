b4e489066b39dfc89e622ac65d536136c155af63
Jameson_UK <1040621+jamesonuk@users.noreply.github.com>
2026-03-10 15:30:06 +0000

    chore: expose media controls enum

--- src/aioamazondevices/__init__.py
+++ src/aioamazondevices/__init__.py
@@ -3,15 +3,17 @@
 __version__ = "13.0.0"
 
 
-from .api import AmazonDevice, AmazonEchoApi
+from .api import AmazonEchoApi
 from .exceptions import (
     CannotAuthenticate,
     CannotConnect,
 )
+from .structures import AmazonDevice, AmazonMediaControls
 
 __all__ = [
     "AmazonDevice",
     "AmazonEchoApi",
+    "AmazonMediaControls",
     "CannotAuthenticate",
     "CannotConnect",
 ]
