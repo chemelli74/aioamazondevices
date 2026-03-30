87460c535262506ef6399c010a5d4cd5455aed21
jamesonuk <1040621+jamesonuk@users.noreply.github.com>
2026-03-30 13:58:07 +0000

    build: rework devcontainer build

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-# Setups the repository.
-
-# Stop on errors
 set -e
 
 POETRY_VERSION="2.3.2" # renovate: depName=poetry datasource=pypi
 
-poetry self update "$POETRY_VERSION"
-poetry env use python3
+echo '[[ -f "$PWD/.venv/bin/activate" ]] && source "$PWD/.venv/bin/activate"' >> ~/.zshrc
+
+pipx install poetry==${POETRY_VERSION}
+pipx install pre-commit
+poetry config virtualenvs.in-project true
 poetry sync --with dev
-poetry run pre-commit install
-poetry run pre-commit install --hook-type commit-msg
-cd
-npm install @commitlint/config-conventional
+pre-commit install
+pre-commit install --hook-type commit-msg
+
+npm install
