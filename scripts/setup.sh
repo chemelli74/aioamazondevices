0629dca0b6a8a46fbf74efb607002e68b805efa4
jamesonuk <1040621+jamesonuk@users.noreply.github.com>
2026-04-01 19:43:35 +0000

    chore: additiojnal up to date build options

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,6 @@ set -e
 
 POETRY_VERSION="2.3.2" # renovate: depName=poetry datasource=pypi
 
-echo '[[ -f "$PWD/.venv/bin/activate" ]] && source "$PWD/.venv/bin/activate"' >> ~/.zshrc
-
 pipx install poetry==${POETRY_VERSION}
 pipx install pre-commit
 poetry config virtualenvs.in-project true
