9202e842d0d52404754f061e7c26654ff1bfa717
Simone Chemelli <simone.chemelli@gmail.com>
2026-04-02 09:58:45 +0000

    chore: tweak

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,8 +7,8 @@ set -e
 UV_VERSION="0.8.22" # renovate: depName=uv datasource=pypi
 
 python3 -m pip install --disable-pip-version-check --no-cache-dir "uv==$UV_VERSION"
-python3 -m uv sync --group dev
-python3 -m uv run pre-commit install
-python3 -m uv run pre-commit install --hook-type commit-msg
+uv sync --group dev
+uv run pre-commit install
+uv run pre-commit install --hook-type commit-msg
 cd
 npm install @commitlint/config-conventional
