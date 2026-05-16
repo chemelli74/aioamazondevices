2e5c4682ca713f42bfbc5fbef0956dc950075bbe
Simone Chemelli <simone.chemelli@gmail.com>
2026-05-16 16:03:00 +0200

    feat: voice history (#789)

--- src/aioamazondevices/api.py
+++ src/aioamazondevices/api.py
@@ -26,6 +26,7 @@ from .const.metadata import (
 )
 from .http_wrapper import AmazonHttpWrapper, AmazonSessionStateData
 from .implementation.dnd import AmazonDnDHandler
+from .implementation.history import AmazonHistoryHandler
 from .implementation.http2 import AmazonHTTP2Client
 from .implementation.notification import AmazonNotificationHandler
 from .implementation.sequence import AmazonSequenceHandler
@@ -37,6 +38,7 @@ from .structures import (
     AmazonMusicProvider,
     AmazonPushMessage,
     AmazonSequenceType,
+    AmazonVocalRecord,
     AmazonVolumeState,
 )
 from .utils import _LOGGER, scrub_fields
@@ -108,7 +110,13 @@ class AmazonEchoApi:
             session_state_data=self._session_state_data,
         )
 
+        self._history_handler = AmazonHistoryHandler(
+            http_wrapper=self._http_wrapper,
+            session_state_data=self._session_state_data,
+        )
+
         self._device_volumes_initialized: bool = False
+        self._voice_history_initialized: bool = False
         self._http2_client: AmazonHTTP2Client | None = None
 
         initial_time = datetime.now(UTC) - timedelta(days=2)  # force initial refresh
@@ -117,6 +125,7 @@ class AmazonEchoApi:
 
         self.on_media_state_event = Signal[dict[str, AmazonMediaState]](self)
         self.on_volume_state_event = Signal[dict[str, AmazonVolumeState]](self)
+        self.on_history_event = Signal[dict[str, AmazonVocalRecord]](self)
 
     @property
     def domain(self) -> str:
@@ -146,15 +155,15 @@ class AmazonEchoApi:
                 "Refreshing devices data after %s",
                 str(timedelta(minutes=round(delta_daily.total_seconds() / 60))),
             )
-            # Request base device data
+            # Request various data that doesn't change that often
             await self._device_handler.get_base_devices()
             await self._media_handler.update_music_providers()
-            # Request routine data
             await self._sequence_handler.update_routines()
 
             self._last_daily_refresh = datetime.now(UTC)
 
         # Only refresh endpoint data if we have no endpoints yet
+        # or if it's been a while since the last refresh
         delta_endpoints = datetime.now(UTC) - self._last_endpoint_refresh
         endpoint_refresh_needed = delta_endpoints >= timedelta(days=1)
         endpoints_recently_checked = delta_endpoints < timedelta(minutes=30)
@@ -229,6 +238,9 @@ class AmazonEchoApi:
             if not self._device_volumes_initialized:
                 await self._media_handler.sync_device_volumes()
                 self._device_volumes_initialized = True
+            if not self._voice_history_initialized:
+                await self._history_handler.get_vocal_history()
+                self._voice_history_initialized = True
             serial = payload.get("dopplerId", {}).get("deviceSerialNumber")
             if serial:
                 volume = AmazonVolumeState(
@@ -236,6 +248,7 @@ class AmazonEchoApi:
                 )
                 self._media_handler.update_cached_device_volume(serial, volume)
             await self._emit_volume_state_event()
+            await self._emit_history_event()
             return
         if event_type == AmazonPushMessage.AudioPlayerState.value:
             if not self._device_handler.devices:
@@ -246,6 +259,7 @@ class AmazonEchoApi:
                 return
             await self._media_handler.sync_media_state(self._device_handler.devices)
             await self._emit_media_state_event()
+            await self._emit_history_event()
             return
 
     async def call_alexa_speak(
@@ -411,4 +425,20 @@ class AmazonEchoApi:
             _LOGGER.debug("Emitting volume state event to subscribers")
             await self.on_volume_state_event.send(
                 await self._media_handler.device_volumes
+            )
+
+    async def sync_history_state(self) -> dict[str, AmazonVocalRecord]:
+        """Sync history state.
+
+        This will be called at startup to sync history state of all devices
+        and can be called later to refresh history state.
+        """
+        return await self._history_handler.get_vocal_history()
+
+    async def _emit_history_event(self) -> None:
+        """Emit vocal history event to subscribers."""
+        if self.on_history_event.frozen:
+            _LOGGER.debug("Emitting vocal history event to subscribers")
+            await self.on_history_event.send(
+                await self._history_handler.get_vocal_history()
             )
